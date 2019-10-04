a2e8e23a0328cc06eb3d7125837ca8f8d4c4f7df
robhercarloz <rhernandezroberto@Gmail.com>
2019-10-04 17:09:07 -0400

    Day5

---
+++ Scripts/jsCodeChallenges.js
@@ -0,0 +1,129 @@
+//Code Challenge 1: MATH 
+//performs function for document
+$(document).ready(function(){
+    //------------------------------------------------------------------
+    //Math Exercise
+
+    //click btn to do math 
+    $("#btnCalculate").click(function(){
+        //store inputs into variables
+        var n1 = Number($("#n1").val());
+        var n2 = Number($("#n2").val());
+        var n3 = Number($("#n3").val());
+        var n4 = Number($("#n4").val());
+        var n5 = Number($("#n5").val());
+        //Do Math
+        var smallest = Math.min(n1,n2,n3,n4,n5);
+        var largest = Math.max(n1,n2,n3,n4,n5);
+        var sum = n1 + n2 + n3 + n4 + n5;
+        var product = n1 * n2 * n3 * n4 * n5;
+        var avg = sum / 5;
+        //display results
+        $("#mathResult").text("The smallest number is: " + smallest + "\nThe largest number is : " + largest + "\nThe average of all 5 is: " + avg
+        + "\nThe sum of the numbers is: " + sum + "\nThe product of the numbers is: " + product);  
+    });
+    //Try again button is clicked
+    $("#btnClear").click(function(){
+        //clear textboxes
+        $("#n1,#n2,#n3,#n4,#n5").val("");
+        //clear result fields
+        $("#mathResult").text("")
+    });
+    //show code
+
+    //----------------------------------------------------------------------
+    //FACTORIAL Exercise
+
+    //Calculate button clicked
+    $("#btnFactorialCalc").click(function(){
+        //DO MATH
+        var fN = Number($("#inputFactorialN").val());
+        //check to see if number is less than 170 otherwise proceed
+        if (fN > 170){
+            alert("Number has to be less than 170 and non-negative.");
+            //$("#inputFactorialN").val("");
+        }
+        //try using recursion to get factorial
+        function factorial(fN){
+            //terminate recursion function
+            if (fN < 0){
+                return;
+            }
+            //base case
+            if (fN === 0){
+                return 1;
+            }
+            //recursion
+            return fN * factorial(fN-1);
+        }
+        //display result
+        $("#rsltFactorial").text("The Factorial for " + fN  + " is: " + factorial(fN));
+    });    
+    //clear button clicked
+    $("#btnClearFactorial").click(function(){
+        $("#inputFactorialN").val("");
+        $("#rsltFactorial").text("");
+    });    
+    //show code
+
+    //----------------------------------------------------------------------
+    //fizzbuzz Exercise
+
+    //fizzbuzz button is clicked
+    $("#btnFizzBuzz").click(function(){
+        //initialize variabales
+        var n1 = $("#fbizzInput1").val();
+        var n2 = $("#fbizzInput2").val();
+        var output = "";
+        //making loop to count 100
+        for(var i = 1; i <= 100; i++){
+            //if statement to check if both inputs have remainders if true concatinate
+            if(i % n1 == 0 && i % n2 == 0){
+                output += "FIZZBUZZ"
+            }else if(i % n1 == 0){
+                output += "Fizz"
+            }else if(i % n2 == 0){
+                output += "buzz"
+            }else{
+                output += i;
+            }
+        }
+        //result
+        $("#rsltFizzBuzz").text(output).val();
+    })
+    //clear button cleared
+    $("#btnFizzBuzzClear").click(function(){
+        $("#fbizzInput1").val("");
+        $("#fbizzInput2").val("");
+        $("#rsltFizzBuzz").text("");
+    });
+    //show code button cleared
+
+    //---------------------------------------------------------------------
+    //Palindrome Exercise
+
+    //palindrome button clicked
+    $("#btnPalindrome").click(function(){
+        //get text to test
+        var word = $("#inputPalindrome").val();
+        function palindrome(word) {
+            //Split, reverse and join string to get reversed text
+            var reversedText  = word.toLowerCase().split('').reverse().join('');
+            //check to see if the word matches first word           
+            return word === reversedText;
+        }
+        //display result
+        $("#rsltPalindrome").text("The word: " + palindrome(word));
+    });
+    //clear button is clicked
+    $("#btnClearPalindrome").click(function(){
+        $("#inputPalindrome").val("");
+        $("#rsltPalindrome").text("");
+    });
+    //show code
+
+});
+
+
+
+
