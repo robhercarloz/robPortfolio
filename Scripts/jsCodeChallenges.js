aea249928490e960accee39865a7d9451121c515
robhercarloz <rhernandezroberto@Gmail.com>
2020-02-12 21:52:03 -0500

    update
    finished Sum of all fears

--- Scripts/jsCodeChallenges.js
+++ Scripts/jsCodeChallenges.js
@@ -27,10 +27,10 @@ $(document).ready(function () {
         $("#rsltLargeN").text("The largest number is: " + largest);
         $("#rsltAvgN").text("The average number is: " + avg);
         $("#rsltSumN").text("The sum number is: " + sum);
-        $("#rsltProductN").text("The Product number is: " + product);        
-    });    
+        $("#rsltProductN").text("The Product number is: " + product);
+    });
     //Try again button is clicked
-    $("#btnClear").click(function(){
+    $("#btnClear").click(function () {
         //clear textboxes
         $("#n1,#n2,#n3,#n4,#n5").val("");
         //clear result fields
@@ -45,7 +45,7 @@ $(document).ready(function () {
         $("#ShowCodeMath").toggle();
         if ($(this).text() == "Show Code") {
             $(this).text("Hide Code");
-        }else {
+        } else {
             $(this).text("Show Code");
         };
     });
@@ -54,35 +54,35 @@ $(document).ready(function () {
     //FACTORIAL Exercise
 
     //Calculate button clicked
-    $("#btnFactorialCalc").click(function(){
+    $("#btnFactorialCalc").click(function () {
         //DO MATH
         var fN = Number($("#inputFactorialN").val());
         //check to see if number is less than 170 otherwise proceed
-        if (fN > 170){
+        if (fN > 170) {
             alert("Number has to be less than 170 and non-negative.");
             //$("#inputFactorialN").val("");
         }
         //try using recursion to get factorial
-        function factorial(fN){
+        function factorial(fN) {
             //terminate recursion function
-            if (fN < 0){
+            if (fN < 0) {
                 return;
             }
             //base case
-            if (fN === 0){
+            if (fN === 0) {
                 return 1;
             }
             //recursion
-            return fN * factorial(fN-1);
+            return fN * factorial(fN - 1);
         }
         //display result
-        $("#rsltFactorial").text("The Factorial for " + fN  + " is: " + factorial(fN));
-    });    
+        $("#rsltFactorial").text("The Factorial for " + fN + " is: " + factorial(fN));
+    });
     //clear button clicked
-    $("#btnClearFactorial").click(function(){
+    $("#btnClearFactorial").click(function () {
         $("#inputFactorialN").val("");
         $("#rsltFactorial").text("");
-    });    
+    });
     //show code
     $("#btnFactorialShowCode").click(function () {
         $("#ShowCodeFactorial").toggle();
@@ -97,25 +97,25 @@ $(document).ready(function () {
     //FIZZBUZZ Exercise
 
     //fizzbuzz button is clicked
-    $("#btnFizzBuzz").click(function(){
+    $("#btnFizzBuzz").click(function () {
         //initialize variabales
         var n1 = $("#fbizzInput1").val();
         var n2 = $("#fbizzInput2").val();
         var output = "";
         //making loop to count 100
-        for (var i = 1; i <= 100; i++){
+        for (var i = 1; i <= 100; i++) {
             var comma = ", ";
             if (i == 100) {
                 comma = "";
             }
             //if statement to check if both inputs have remainders if true concatinate
-            if(i % n1 == 0 && i % n2 == 0){
+            if (i % n1 == 0 && i % n2 == 0) {
                 output += "<span class='FizzBuzz'>FizzBuzz" + comma + "</span>";
-            }else if(i % n1 == 0){
+            } else if (i % n1 == 0) {
                 output += "<span class='Fizz'>Fizz" + comma + "</span>";
-            }else if(i % n2 == 0){
+            } else if (i % n2 == 0) {
                 output += "<span class='Buzz'>Buzz" + comma + "</span>";
-            }else{
+            } else {
                 output += i + comma;
             }
         }
@@ -126,7 +126,7 @@ $(document).ready(function () {
         $("#rsltFizzBuzz").html(output);
     })
     //clear button cleared
-    $("#btnFizzBuzzClear").click(function(){
+    $("#btnFizzBuzzClear").click(function () {
         $("#fbizzInput1").val("");
         $("#fbizzInput2").val("");
         $("#rsltFizzBuzz").text("");
@@ -145,7 +145,7 @@ $(document).ready(function () {
 
     //palindrome button clicked
     $("#btnPalindrome").click(function () {
-       
+
         //get word
         var word = $("#inputPalindrome").val();
         //function to check word
@@ -165,9 +165,9 @@ $(document).ready(function () {
         }
         //display result
         $("#rsltPalindrome").text("The word " + word + " " + torFalse + " a palindrome.");
-    });    
+    });
     //clear button is clicked
-    $("#btnClearPalindrome").click(function(){
+    $("#btnClearPalindrome").click(function () {
         $("#inputPalindrome").val("");
         $("#rsltPalindrome").text("");
     });
@@ -192,26 +192,72 @@ $(document).ready(function () {
         var indexTwo = + $("#index2").val();
         var indexThree = + $("#index3").val();
         var indexFour = + $("#index4").val();
+        var kvalue = + $("#kvalue").val();
+        //Condition to check the boxes are not empty
+        if (indexZero != null && indexOne != null && indexTwo != null && indexThree != null && indexFour != null && kvalue) {
+            //make an array to hold all values
+            var soaFears = [
+                indexZero,
+                indexOne,
+                indexTwo,
+                indexThree,
+                indexFour
+            ];
+            //looping in the array to see if any match
+            for (var i = 0; i < 4; i++) {
+                //going through array and taking final value for comparing sums
+                var pulledNumber = soaFears.pop();
+                console.log('The array just popped ' + pulledNumber + ' to compare.')
+                //remaining array values shown in console
+                console.log('Your remaining array values are:')
+                console.log(soaFears);
+                //add pulled value to remaining array indexes
+                console.log('Adding pulled number to array:')
+                var modifiedArray = soaFears.map(addPulled)
 
-        //make an array to hold all values
-        var soaFears = [
-            indexZero,
-            indexOne,
-            indexTwo,
-            indexThree,
-            indexFour
-        ];
+                function addPulled(num) {
+                    return num + pulledNumber;
 
-        //looping in the array to see if any match
+                }
+
+                console.log(modifiedArray);
+
+                //find funct
+                function doesKExist(modifiedArray) {
+                    return modifiedArray == kvalue;
+                }
+
+                //store sum being searched in variable
+                var kWasFound = modifiedArray.find(doesKExist);
+                console.log(kWasFound);
 
 
-    })
+                if (kWasFound === kvalue) {
+                    break;
+                }
+            }
+            //show result in output field
+            if (kvalue === kWasFound) {
+                $("#soafResult").text("Found match for k!")
+                console.log("K Exists!")
+            } else {
+                $("#soafResult").text("There are no matches for k.")
+                console.log("K is not found.")
+            }
+        } else {
+            $("#soafResult").text("Error: please fill in all empty textboxes!")
+            
+        }
+        
+    });
     //Generate numbers
-    $("#btnGenerate").click(function () {
+    $("#btnGenerateSOAF").click(function () {
         //get id of the textbox and assign it each a random number
-        $("#index0").text(Math.floor(Math.random() * 100) + 1);
-
-
+        $("#index0").val(Math.floor(Math.random() * 200) + 1);
+        $("#index1").val(Math.floor(Math.random() * 200) + 1);
+        $("#index2").val(Math.floor(Math.random() * 200) + 1);
+        $("#index3").val(Math.floor(Math.random() * 200) + 1);
+        $("#index4").val(Math.floor(Math.random() * 200) + 1);
     })
     //Clear textfields
     $("#btnClearSOAF").click(function () {
@@ -221,7 +267,7 @@ $(document).ready(function () {
         $("#index3").val('');
         $("#index4").val('');
         $("#kvalue").val('');
-
+        $("#soafResult").text("");
     });
 
 
